6e6202061d394b106330cb516726329fb37802a8
Karol <kpulawski13@gmail.com>
2023-12-06 20:32:40 +0000

    Handle when fetch fails

--- src/hooks/useDataFetching.jsx
+++ src/hooks/useDataFetching.jsx
@@ -2,15 +2,24 @@ import { useEffect, useState } from 'react';
 
 function useDataFetching() {
   const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch('https://fakestoreapi.com/products?limit=5')
-      .then((response) => response.json())
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
+        }
+        return response.json();
+      })
       .then((response) => {
         setData(response);
+      })
+      .catch((error) => {
+        setError(error);
       });
   }, []);
-  return { data };
+  return { data, error };
 }
 
 export default useDataFetching;
