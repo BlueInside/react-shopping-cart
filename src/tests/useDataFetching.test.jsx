e0a0e920d0a9fb9a973a1e9280e57829dc92999d
Karol <kpulawski13@gmail.com>
2023-12-06 20:41:19 +0000

    Tests that initialize with correct state

--- src/tests/useDataFetching.test.jsx
+++ src/tests/useDataFetching.test.jsx
@@ -3,7 +3,16 @@ import { renderHook, waitFor } from '@testing-library/react';
 import useDataFetching from '../hooks/useDataFetching';
 
 describe('useDataFetching', () => {
+  it('initializes with correct initial state', () => {
+    // Render useDataFetch
+    const { result } = renderHook(useDataFetching);
+
+    expect(result.current.data).toBeNull();
+    expect(result.current.error).toBeNull();
+  });
+
   it('returns data when fetch is successful', async () => {
+    // Mock fetch with resolved promise
     globalThis.fetch = vi.fn(() =>
       Promise.resolve({
         ok: true,
@@ -13,7 +22,15 @@ describe('useDataFetching', () => {
       })
     );
 
-    const { result } = renderHook(useDataFetching);
+    const { result, rerender } = renderHook(useDataFetching);
+
+    //Wait for data to load
+    await waitFor(() => {
+      expect(result.current.data).toEqual({ data: 'mockData' });
+    });
+
+    // Render it again
+    rerender();
 
     await waitFor(() => {
       expect(result.current.data).toEqual({ data: 'mockData' });
@@ -21,12 +38,15 @@ describe('useDataFetching', () => {
   });
 
   it('return error when fetch fails', async () => {
+    // Mock rejected fetch
     globalThis.fetch = vi.fn(() =>
       Promise.reject(new Error('Failed to fetch data'))
     );
 
     const { result } = renderHook(useDataFetching);
     expect(result.current.error).toBeNull();
+
+    // Wait for data to load
     await waitFor(() => {
       expect(result.current.error).toEqual(new Error('Failed to fetch data'));
     });
