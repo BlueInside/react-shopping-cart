43b2f334e664161bf37eb925c19a934735b36969
Karol <kpulawski13@gmail.com>
2023-12-10 07:19:10 +0000

    test loading state

--- src/tests/useDataFetching.test.jsx
+++ src/tests/useDataFetching.test.jsx
@@ -51,4 +51,45 @@ describe('useDataFetching', () => {
       expect(result.current.error).toEqual(new Error('Failed to fetch data'));
     });
   });
+
+  it('loading value starts as true, change to false when fetch is successful', async () => {
+    // Mock fetch with resolved promise
+    globalThis.fetch = vi.fn(() =>
+      Promise.resolve({
+        ok: true,
+        json: () => {
+          return Promise.resolve({ data: 'mockData' });
+        },
+      })
+    );
+
+    const { result } = renderHook(useDataFetching);
+    expect(result.current.data).toBeNull();
+    expect(result.current.error).toBeNull();
+    expect(result.current.loading).toBeTruthy();
+
+    await waitFor(() => {
+      expect(result.current.loading).toBeFalsy();
+      expect(result.current.data).not.toBeNull();
+      expect(result.current.error).toBeNull();
+    });
+  });
+
+  it('loading value starts as true, change to false when fetch is unsuccessful', async () => {
+    // Mock fetch with resolved promise
+    globalThis.fetch = vi.fn(() =>
+      Promise.reject(new Error('Failed to fetch data'))
+    );
+
+    const { result } = renderHook(useDataFetching);
+    expect(result.current.data).toBeNull();
+    expect(result.current.error).toBeNull();
+    expect(result.current.loading).toBeTruthy();
+
+    await waitFor(() => {
+      expect(result.current.loading).toBeFalsy();
+      expect(result.current.data).toBeNull();
+      expect(result.current.error).toEqual(new Error('Failed to fetch data'));
+    });
+  });
 });
