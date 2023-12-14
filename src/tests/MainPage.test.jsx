c4c589c7555fe1973021936955d8cb2a5043eb2d
Karol <kpulawski13@gmail.com>
2023-12-14 17:37:58 +0000

    test loading and error para

--- src/tests/MainPage.test.jsx
+++ src/tests/MainPage.test.jsx
@@ -1,6 +1,7 @@
-import { render, screen } from '@testing-library/react';
-import { describe, expect, it } from 'vitest';
+import { render, screen, waitFor } from '@testing-library/react';
+import { describe, expect, it, vi } from 'vitest';
 import MainPage from '../components/MainPage';
+
 describe('MainPage', () => {
   it('renders MainPage component correctly', () => {
     const { container } = render(<MainPage />);
@@ -21,9 +22,33 @@ describe('MainPage', () => {
     expect(button).toBeInTheDocument();
   });
 
-  it('should fetch and display 3 images', () => {
+  it('displays loading paragraph', () => {
     render(<MainPage />);
-    const images = screen.getAllByRole('img');
-    expect(images).toHaveLength(3);
+    const loadingPara = screen.getByRole('status', { value: /Loading.../i });
+    expect(loadingPara).toBeInTheDocument();
+  });
+
+  it('should fetch and display 3 images', async () => {
+    render(<MainPage />);
+    await waitFor(() => {
+      const images = screen.getAllByRole('img');
+      expect(images).toHaveLength(3);
+    });
+  });
+
+  it('displays error paragraph', () => {
+    globalThis.fetch = vi.fn(() => {
+      Promise.resolve({
+        ok: false,
+        json: () => {
+          return Promise.reject(new Error('Failed to fetch data'));
+        },
+      });
+      render(<MainPage />);
+      const errorPara = screen.getByRole('error', {
+        value: /Failed to fetch data/i,
+      });
+      expect(errorPara).toBeInTheDocument();
+    });
   });
 });
