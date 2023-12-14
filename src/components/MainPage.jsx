2f47d6a061128833f56236bdf318c3bfd06ee406
Karol <kpulawski13@gmail.com>
2023-12-14 17:37:37 +0000

    display conditionally error, loading or img

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,8 +1,18 @@
 import Button from '../components/Button';
+import useDataFetching from '../hooks/useDataFetching';
 function MainPage() {
+  const { data, error, loading } = useDataFetching(3);
+  if (error) return <p role="error">Error</p>;
   return (
     <main>
       <h1>Trend Tribe: Join the Fashion Revolution</h1>
+      <div>
+        {loading && <p role="status">Loading...</p>}
+        {data &&
+          data.map((item) => {
+            return <img key={item.id} src="#" />;
+          })}
+      </div>
       <Button label={'Discover more'} />
     </main>
   );
