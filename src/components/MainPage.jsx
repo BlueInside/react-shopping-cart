dfcea3ba1edc56ff3f072d92895758eb943bcb0c
Karol <kpulawski13@gmail.com>
2023-12-14 16:17:55 +0000

    Created MainPage component with header and btn

---
+++ src/components/MainPage.jsx
@@ -0,0 +1,11 @@
+import Button from '../components/Button';
+function MainPage() {
+  return (
+    <main>
+      <h1>Trend Tribe: Join the Fashion Revolution</h1>
+      <Button label={'Discover more'} />
+    </main>
+  );
+}
+
+export default MainPage;
