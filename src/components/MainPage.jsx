bbbe2cc4b174d6beceb8f7b089971c29b8c3c0ab
Karol <kpulawski13@gmail.com>
2023-12-15 16:39:53 +0100

    src changed

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -3,8 +3,8 @@ import useDataFetching from '../hooks/useDataFetching';
 
 function MainPage() {
   const { data, error, loading } = useDataFetching(3);
-  if (error) return <p role="error">Error</p>;
 
+  if (error) return <p role="error">Error</p>;
   return (
     <main>
       <h1>Trend Tribe: Join the Fashion Revolution</h1>
@@ -12,7 +12,7 @@ function MainPage() {
         {loading && <p role="status">Loading...</p>}
         {data &&
           data.map((item) => {
-            return <img key={item.id} src="#" />;
+            return <img key={item.id} src={item.image} />;
           })}
       </div>
       <Button label={'Discover more'} />
