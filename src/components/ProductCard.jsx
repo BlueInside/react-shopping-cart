feec9227ffd1b6be724bb1bb0c94231d4f395cf6
Karol <kpulawski13@gmail.com>
2023-12-21 07:51:47 +0100

    Add buttons and functions that change quantity

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,11 +1,47 @@
 import PropTypes from 'prop-types';
 import Button from './Button';
+import { useState } from 'react';
+
 function ProductCard({ image, title, description, onClick }) {
+  const [quantity, setQuantity] = useState(0);
+
+  function handleChange(event) {
+    const inputValue = event.target.value;
+
+    if (Number(inputValue)) {
+      setQuantity(parseInt(inputValue, 10));
+    } else if (!isNaN(inputValue)) {
+      setQuantity(0);
+    }
+  }
+
+  function handleIncrement() {
+    setQuantity((q) => q + 1);
+  }
+
+  function handleReduce() {
+    if (quantity - 1 >= 0) {
+      setQuantity((q) => q - 1);
+    } else return;
+  }
+
   return (
     <div role="productCard">
       <img onClick={onClick} src={image} alt="item" role="productImage" />
       <p role="productTitle">{title}</p>
-      <Button handleClick={handleClick} label={'+'} role="add" />
+      <div>
+        <Button handleClick={handleIncrement} label={'+'} role="add" />
+        <label htmlFor="quantity" />
+        <input
+          type="number"
+          id="quantity"
+          role="quantity"
+          value={quantity}
+          min="0"
+          onChange={handleChange}
+        />
+        <Button handleClick={handleReduce} label={'-'} role={'remove'} />
+      </div>
     </div>
   );
 }
