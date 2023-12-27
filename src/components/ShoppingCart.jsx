cd5ce9e98f2672790d49ea28c33198d3686d6d87
Karol <kpulawski13@gmail.com>
2023-12-27 10:35:25 +0100

    added delete button

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import { useState } from 'react';
 import Button from './Button';
-function CartItem({ image, title, price, quantity = 1 }) {
+function CartItem({ image, title, price, quantity = 1, handleDeleteCartItem }) {
   const [qty, setQty] = useState(quantity);
 
   function handleAddQty() {
@@ -26,6 +26,11 @@ function CartItem({ image, title, price, quantity = 1 }) {
           handleClick={handleRemoveQty}
         />
       </div>
+      <Button
+        label={'Delete Icon'}
+        role={'deleteCartItem'}
+        handleClick={handleDeleteCartItem}
+      />
     </div>
   );
 }
@@ -34,20 +39,31 @@ CartItem.propTypes = {
   image: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   price: PropTypes.number.isRequired,
+  handleDeleteCartItem: PropTypes.func.isRequired,
   quantity: PropTypes.number,
 };
 
 CartItem.defaultProps = {};
 function ShoppingCart({ products }) {
+  const [cartProducts, setCartProducts] = useState(products);
+
+  function handleDeleteCartItem(productId) {
+    const updatedArray = cartProducts.filter(
+      (product) => product.id !== productId
+    );
+    setCartProducts(updatedArray);
+  }
+
   return (
     <div>
-      {products.map((product) => (
+      {cartProducts.map((product) => (
         <CartItem
           key={product.id}
           title={product.title}
           price={product.price}
           quantity={product.quantity}
           image={product.image}
+          handleDeleteCartItem={() => handleDeleteCartItem(product.id)}
         />
       ))}
     </div>
