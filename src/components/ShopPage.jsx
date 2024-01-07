aa6b901b11861a2dd981d23004ff64b36bbb2dea
Karol <kpulawski13@gmail.com>
2024-01-07 18:33:54 +0000

    Shop page test and implementation display Products

--- src/components/ShopPage.jsx
+++ src/components/ShopPage.jsx
@@ -1,5 +1,27 @@
+import { useEffect } from 'react';
+import useDataFetching from '../hooks/useDataFetching';
+import ProductCard from './ProductCard';
+
 function ShopPage() {
-  return <></>;
+  const { data, loading, error } = useDataFetching(9);
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error</div>;
+  return (
+    <div>
+      {data.map((product) => (
+        <ProductCard
+          key={product.id}
+          image={product.image}
+          price={product.price}
+          title={product.title}
+          description={product.description}
+          onClick={() => {
+            console.log('click');
+          }}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default ShopPage;
