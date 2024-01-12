314851d285894d0ab8edf95b48946a048efeeec6
Karol <kpulawski13@gmail.com>
2024-01-12 16:01:05 +0000

    clean comments

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -15,29 +15,8 @@ function ProductCard({ image, price, title, description, rating, category }) {
     if (!isModalOpen) setIsModalOpen(true);
   }
 
-  function closeProductModal() {
-    if (isModalOpen) setIsModalOpen(false);
-  }
-
-  // function handleChange(event) {
-  //   const inputValue = event.target.value;
-  //   const numericValue = Number(inputValue);
-
-  //   if (!isNaN(numericValue)) {
-  //     setQuantity(parseInt(inputValue, 10));
-  //   } else {
-  //     setQuantity(quantity);
-  //   }
-  // }
-
-  // function handleIncrement() {
-  //   setQuantity((q) => q + 1);
-  // }
-
-  // function handleReduce() {
-  //   if (quantity - 1 >= 0) {
-  //     setQuantity((q) => q - 1);
-  //   } else return;
+  // function closeProductModal() {
+  //   if (isModalOpen) setIsModalOpen(false);
   // }
 
   if (isModalOpen) {
@@ -49,9 +28,7 @@ function ProductCard({ image, price, title, description, rating, category }) {
         title={title}
         category={category}
         rating={rating}
-        // quantity={quantity}
-        // handleIncrement={handleIncrement}
-        // handleReduce={handleReduce}
+        handleAddToCart={handleAddToCart}
       />
     );
   }
@@ -68,19 +45,6 @@ function ProductCard({ image, price, title, description, rating, category }) {
         {title}
       </p>
       <p role="productPrice">{price}</p>
-      <div>
-        {/* <Button handleClick={handleIncrement} label={'+'} role="add" /> */}
-        {/* <label htmlFor="quantity" />
-        <input
-          type="number"
-          id="quantity"
-          role="quantity"
-          value={quantity}
-          min="0"
-          onChange={handleChange}
-        /> */}
-        {/* <Button handleClick={handleReduce} label={'-'} role={'remove'} /> */}
-      </div>
       <Button
         role={'addToCart'}
         handleClick={handleAddToCart}
