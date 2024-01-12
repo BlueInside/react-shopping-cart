517b5bd7fe35327835bb6bccd51807f7e538d536
Karol <kpulawski13@gmail.com>
2024-01-12 16:01:21 +0000

    test quantity

--- src/tests/DisplayProductInformation.test.jsx
+++ src/tests/DisplayProductInformation.test.jsx
@@ -19,12 +19,16 @@ describe('DisplayProductInformation component', () => {
   });
 
   it('renders DisplayProductInformation component correctly', () => {
-    const { container } = render(<DisplayProductInformation {...product} />);
+    const { container } = render(
+      <DisplayProductInformation {...product} handleAddToCart={() => {}} />
+    );
     expect(container).toMatchSnapshot();
   });
 
   it('displays product details accordingly to product object', async () => {
-    render(<DisplayProductInformation {...product} />);
+    render(
+      <DisplayProductInformation {...product} handleAddToCart={() => {}} />
+    );
 
     expect(screen.getByRole('productImage')).toHaveAttribute(
       'src',
@@ -37,29 +41,48 @@ describe('DisplayProductInformation component', () => {
     expect(screen.getByRole('price')).toHaveTextContent(product.price);
     expect(screen.getByRole('rating')).toHaveTextContent(product.rating.rate);
     expect(screen.getByRole('category')).toHaveTextContent(product.category);
+    expect(screen.getByRole('addToCart')).toBeInTheDocument();
   });
 
   it('let user adjust quantity', async () => {
     const user = userEvent.setup();
 
-    render(<DisplayProductInformation {...product} />);
-    const quantity = screen.getByRole('quantity');
+    render(
+      <DisplayProductInformation {...product} handleAddToCart={() => {}} />
+    );
+    const quantityInput = screen.getByRole('quantity');
     const addBtn = screen.getByRole('add');
-
-    // Initial quantity is 0
-    expect(quantity).toHaveTextContent('0');
+    const reduceBtn = screen.getByRole('reduce');
+    // Initial quantity is 1
+    expect(quantityInput).toHaveValue(1);
 
     // Clicking remove when the quantity is already 0 should not decrease it
-    await user.click(screen.getByRole('reduce'));
-    expect(quantity).toHaveTextContent('0');
+    await user.click(reduceBtn);
+    expect(quantityInput).toHaveValue(1);
 
     // // Clicking add should increase the quantity to 1
     await user.click(addBtn);
-    expect(quantity).toHaveTextContent('1');
+    expect(quantityInput).toHaveValue(2);
 
     // Clicking add twice should increase the quantity to 3
     await user.click(addBtn);
     await user.click(addBtn);
-    expect(quantity).toHaveTextContent('3');
+    expect(quantityInput).toHaveValue(4);
+
+    // When user clears input it should show 1
+    await user.clear(quantityInput);
+    expect(quantityInput).toHaveValue(1);
+
+    // Checks if inputs updates value correctly
+    await user.type(quantityInput, '234');
+    expect(quantityInput).toHaveValue(1234);
+
+    // Checks if input deletes numbers properly
+    await user.type(quantityInput, '{backspace}{backspace}{backspace}');
+    expect(quantityInput).toHaveValue(1);
+
+    // Ignores letters
+    await user.type(quantityInput, '234abc');
+    expect(quantityInput).toHaveValue(1234);
   });
 });
