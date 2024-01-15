816d527e4c043a0e8c9d64c35838bc173797f692
Karol <kpulawski13@gmail.com>
2024-01-15 16:23:45 +0000

    corrected app test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,10 +1,11 @@
 import { describe, it, expect } from 'vitest';
 import App from './App';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 
 describe('App', () => {
-  it('renders heading', () => {
-    const { container } = render(<App />);
-    expect(container).toBeInTheDocument();
+  it('renders Navbar', () => {
+    render(<App />, { wrapper: BrowserRouter });
+    expect(screen.getByRole('navigation')).toBeInTheDocument();
   });
 });
