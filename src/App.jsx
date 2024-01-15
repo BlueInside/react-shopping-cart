ce614711bb6096502a6691b6c593761101985e51
Karol <kpulawski13@gmail.com>
2024-01-15 16:23:34 +0000

    added navbar and outlet

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
-// import { useState } from 'react';
-
+import { Outlet } from 'react-router-dom';
+import Navbar from './components/Navbar';
 function App() {
-  return <h1>My-shopping cart</h1>;
+  const navList = [
+    {
+      path: 'main',
+      label: 'Main page',
+    },
+    {
+      path: 'shop',
+      label: 'Shop',
+    },
+    {
+      path: 'cart',
+      label: 'Cart',
+    },
+  ];
+  return (
+    <div>
+      <Navbar links={navList} />
+      <Outlet />
+    </div>
+  );
 }
 
 export default App;
