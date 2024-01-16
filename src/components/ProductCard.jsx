000bfa984edf75f7881836109fa5a6880a9d61b9
Karol <kpulawski13@gmail.com>
2024-01-16 13:27:31 +0000

    removed unnecessary code

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,54 +1,32 @@
 import PropTypes from 'prop-types';
-import { useState } from 'react';
 import Button from './Button';
-import DisplayProductInformation from './DisplayProductInformation';
 import { addToCard } from '../utils/cart';
-function ProductCard({
-  product,
-  image,
-  price,
-  title,
-  description,
-  rating,
-  category,
-}) {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  function showProductModal() {
-    if (!isModalOpen) setIsModalOpen(true);
-  }
-
-  if (isModalOpen) {
-    return (
-      <DisplayProductInformation
-        image={image}
-        description={description}
-        price={price}
-        title={title}
-        category={category}
-        rating={rating}
-        handleAddToCart={(product, qty) => addToCard(product, qty)}
-      />
-    );
-  }
+import { useNavigate } from 'react-router-dom';
+function ProductCard({ product, image, price, title }) {
+  const navigate = useNavigate();
 
   return (
-    <div role="productCard">
-      <img
-        onClick={showProductModal}
-        src={image}
-        alt="item"
-        role="productImage"
-      />
-      <p onClick={showProductModal} role="productTitle">
-        {title}
-      </p>
-      <p role="productPrice">{price}</p>
-      <Button
-        role={'addToCart'}
-        handleClick={() => addToCard(product, 1)}
-        label={'addToCart'}
-      />
+    <div>
+      <div role="productCard">
+        <img
+          onClick={() => navigate(`/shop/products/${product.id}`)}
+          src={image}
+          alt="item"
+          role="productImage"
+        />
+        <p
+          onClick={() => navigate(`/shop/products/${product.id}`)}
+          role="productTitle"
+        >
+          {title}
+        </p>
+        <p role="productPrice">{price}</p>
+        <Button
+          role={'addToCart'}
+          handleClick={() => addToCard(product, 1)}
+          label={'addToCart'}
+        />
+      </div>
     </div>
   );
 }
