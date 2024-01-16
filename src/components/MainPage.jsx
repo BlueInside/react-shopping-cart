f6b9bb64c9ab6505a278ec078cb921645a80fa1b
Karol <kpulawski13@gmail.com>
2024-01-16 14:54:57 +0000

    added navigate to button

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,21 +1,23 @@
+import { useNavigate } from 'react-router-dom';
 import Button from '../components/Button';
 import useDataFetching from '../hooks/useDataFetching';
 
 function MainPage() {
+  const navigate = useNavigate();
   const { data, error, loading } = useDataFetching();
 
+  if (loading || !data) return <p role="loader">Loading...</p>;
   if (error) return <p role="error">Error</p>;
   return (
     <main>
       <h1>Trend Tribe: Join the Fashion Revolution</h1>
       <div>
-        {loading && <p role="status">Loading...</p>}
         {data &&
           data.slice(0, 3).map((item) => {
             return <img key={item.id} src={item.image} />;
           })}
       </div>
-      <Button label={'Discover more'} />
+      <Button handleClick={() => navigate('/shop')} label={'Discover more'} />
     </main>
   );
 }
