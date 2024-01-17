1dc55a17f2c33341c88898023f803085f7d8bee7
Karol <kpulawski13@gmail.com>
2024-01-17 16:16:19 +0000

    small adjustments

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import { useState } from 'react';
 function App() {
+  const [navbarItemsCount, setNavbarItemsCount] = useState(0);
   const navList = [
     {
       path: 'main',
