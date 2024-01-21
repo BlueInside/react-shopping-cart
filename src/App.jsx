16df3c8b930883a9d3be08ff7f24bdf55a41cd6e
Karol <kpulawski13@gmail.com>
2024-01-21 12:22:58 +0000

    added Theme provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import { ThemeProvider } from 'styled-components';
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import { Container } from './components/styles/Container.styled';
+
+import './App.css';
+
+const theme = {
+  colors: {
+    primaryBlue: '#007BFF',
+    primaryGreen: '#28A745',
+    primaryYellow: '#FFC107',
+    secondaryGrey: '#F8F9FA',
+    secondaryDarkGrey: '#6C757D',
+    secondaryWhite: '#FFFFFF',
+    offWhite: '#F5F5F5',
+    black: '#000000',
+    errorRed: '#DC3545',
+  },
+};
+
 function App() {
   const navList = [
     {
@@ -16,10 +35,14 @@ function App() {
     },
   ];
   return (
-    <div>
-      <Navbar links={navList} />
-      <Outlet />
-    </div>
+    <ThemeProvider theme={theme}>
+      <>
+        <Navbar links={navList} />
+        <Container>
+          <Outlet />
+        </Container>
+      </>
+    </ThemeProvider>
   );
 }
 
