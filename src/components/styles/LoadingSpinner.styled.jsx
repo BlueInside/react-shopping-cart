e86dc42fa04ec1c92eb293218f20519fb844cb7c
Karol <kpulawski13@gmail.com>
2024-01-21 13:01:58 +0000

    created spinner

---
+++ src/components/styles/LoadingSpinner.styled.jsx
@@ -0,0 +1,28 @@
+import styled, { keyframes } from 'styled-components';
+
+const spin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to{
+    transform: rotate(360deg)
+  }
+`;
+
+const LoadingSpinner = styled.div`
+  width: 60px;
+  height: 60px;
+  margin-left: auto;
+  margin-right: auto;
+  left: 0px;
+  right: 0px;
+  top: 50%;
+  text-align: center;
+  position: absolute;
+  border: 8px solid ${({ theme }) => theme.colors.secondaryDarkGrey};
+  border-top-color: ${({ theme }) => theme.colors.black};
+  border-radius: 50%;
+  animation: ${spin} 1.5s linear infinite;
+`;
+
+export default LoadingSpinner;
