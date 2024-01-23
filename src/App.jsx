b6f45b70e6ec114ccdb4e421f3184d0dc0b8148b
Karol <kpulawski13@gmail.com>
2024-01-23 09:28:55 +0000

    import theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,10 @@
 import { ThemeProvider } from 'styled-components';
+import theme from './components/styles/theme.';
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import { Container } from './components/styles/Container.styled';
 
 import './App.css';
-
-const theme = {
-  colors: {
-    primaryBlue: '#007BFF',
-    primaryGreen: '#28A745',
-    primaryYellow: '#FFC107',
-    secondaryGrey: '#F8F9FA',
-    secondaryDarkGrey: '#6C757D',
-    secondaryWhite: '#FFFFFF',
-    offWhite: '#F5F5F5',
-    black: '#000000',
-    errorRed: '#DC3545',
-  },
-};
 
 function App() {
   const navList = [
